7c86e6a1fdd8cf5c2b33575e29026742d7683678
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-08-25 17:38:06 +0200

    Update to Micronaut 3.0.0

--- src/main/java/com/example/LoggerDelegate.java
+++ src/main/java/com/example/LoggerDelegate.java
@@ -1,11 +1,10 @@
 package com.example;
 
+import jakarta.inject.Singleton;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.inject.Singleton;
 
 @Singleton
 public class LoggerDelegate implements JavaDelegate {
